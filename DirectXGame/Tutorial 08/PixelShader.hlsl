8d2f2180f2e5cc23fa7d590166987864cf602908
Jared Mathes <jaredmathes@shaw.ca>
2019-08-18 21:05:00 -0600

    Tutorial 08 - Pixel Shaders

---
+++ DirectXGame/Tutorial 08/PixelShader.hlsl
@@ -0,0 +1,16 @@
+struct PS_INPUT
+{
+	float4 position: POSITION;
+	float3 color: COLOR;
+};
+
+struct PS_OUTPUT
+{
+	float4 position: SV_POSITION;
+	float3 color: COLOR;
+};
+
+float4 psmain(PS_INPUT input) : SV_TARGET
+{
+	return float4(input.color, 1.0f);
+}
