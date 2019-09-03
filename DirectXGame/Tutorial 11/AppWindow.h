d033fc7985a8ef55781e96b22270c5e71db10528
Jared Mathes <jaredmathes@shaw.ca>
2019-09-02 18:09:49 -0600

    Tutorial 11

---
+++ DirectXGame/Tutorial 11/AppWindow.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Window.h"
+#include "GraphicsEngine.h"
+#include "SwapChain.h"
+#include "DeviceContext.h"
+#include "VertexBuffer.h"
+#include "ConstantBuffer.h"
+#include "VertexShader.h"
+#include "PixelShader.h"
+#include "IndexBuffer.h"
+
+class AppWindow : public Window
+{
+public:
+	AppWindow() = default;
+
+	void updateQuadPosition();
+
+	// Inherited via Window
+	virtual void onCreate() override;
+	virtual void onUpdate() override;
+	virtual void onDestroy() override;
+
+private:
+	SwapChain* m_swap_chain;
+	VertexBuffer* m_vb;
+	VertexShader* m_vs;
+	PixelShader* m_ps;
+	ConstantBuffer* m_cb;
+	IndexBuffer* m_ib;
+
+	long m_old_delta;
+	long m_new_delta;
+	float m_delta_time;
+	float m_delta_pos;
+	float m_delta_scale;
+};
