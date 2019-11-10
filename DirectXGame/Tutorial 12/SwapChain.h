da9f2d64b2950d3a73a65b01bf0f1a823d628594
Jared Mathes <jared.c.mathes@gmail.com>
2019-11-10 16:20:52 -0700

    Tutorial 12 - Baseline

---
+++ DirectXGame/Tutorial 12/SwapChain.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <d3d11.h>
+
+class DeviceContext;
+
+class SwapChain
+{
+public:
+	SwapChain();
+	bool init(HWND hwnd, UINT width, UINT height);
+	bool present(bool vsync);
+	bool release();
+	~SwapChain();
+
+private:
+	IDXGISwapChain* m_swap_chain;
+	ID3D11RenderTargetView* m_rtv;
+	ID3D11DepthStencilView* m_dsv;
+
+	friend class DeviceContext;
+};
+
