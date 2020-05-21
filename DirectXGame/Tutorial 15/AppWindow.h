66d267f44e79eba821439dfa97755d9609fb8c69
Jared Mathes <jared.c.mathes@gmail.com>
2020-05-21 16:30:28 -0600

    Tutorial 15 - Architecture overhall (Render System, RAII Principles, Exceptions)

---
+++ DirectXGame/Tutorial 15/AppWindow.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Window.h"
+#include "GraphicsEngine.h"
+#include "SwapChain.h"
+#include "DeviceContext.h"
+#include "VertexBuffer.h"
+#include "ConstantBuffer.h"
+#include "VertexShader.h"
+#include "PixelShader.h"
+#include "IndexBuffer.h"
+#include "InputListener.h"
+#include "Matrix4x4.h"
+
+class AppWindow : public Window, public InputListener
+{
+public:
+	AppWindow() = default;
+
+	void update();
+
+	// Inherited via Window
+	virtual void onCreate() override;
+	virtual void onUpdate() override;
+	virtual void onDestroy() override;
+	
+	// Inherited via InputListener
+	virtual void onKeyDown(int key) override;
+	virtual void onKeyUp(int key) override;
+	virtual void onMouseMove(const Point& mouse_pos) override;
+	virtual void onFocus() override;
+	virtual void onKillFocus() override;
+	virtual void onLeftMouseDown(const Point& mouse_pos) override;
+	virtual void onRightMouseDown(const Point& mouse_pos) override;
+	virtual void onLeftMouseUp(const Point& mouse_pos) override;
+	virtual void onRightMouseUp(const Point& mouse_pos) override;
+
+private:
+	SwapChain* m_swap_chain;
+	VertexBuffer* m_vb;
+	VertexShader* m_vs;
+	PixelShader* m_ps;
+	ConstantBuffer* m_cb;
+	IndexBuffer* m_ib;
+
+	long m_old_delta;
+	long m_new_delta;
+	float m_delta_time;
+	float m_delta_pos;
+	float m_delta_scale;
+
+	float m_rot_x = 0.0f;
+	float m_rot_y = 0.0f;
+
+	float m_scale_cube = 1.0f;
+	float m_forward = 0.0f;
+	float m_rightward = 0.0f;
+
+	Matrix4x4 m_world_cam;
+};
---
+++ DirectXGame/Tutorial 15/GraphicsEngine.cpp
@@ -0,0 +1,35 @@
+#include "GraphicsEngine.h"
+#include "RenderSystem.h"
+
+GraphicsEngine::GraphicsEngine()
+{
+}
+
+bool GraphicsEngine::init()
+{
+	m_render_system = new RenderSystem();
+	m_render_system->init();
+	return true;
+}
+
+bool GraphicsEngine::release()
+{
+	m_render_system->release();
+	delete m_render_system;
+	return true;
+}
+
+RenderSystem* GraphicsEngine::getRenderSystem()
+{
+	return m_render_system;
+}
+
+GraphicsEngine::~GraphicsEngine()
+{
+}
+
+GraphicsEngine * GraphicsEngine::get()
+{
+	static GraphicsEngine engine;
+	return &engine;
+}
